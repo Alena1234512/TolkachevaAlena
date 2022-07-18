b1bbfefa05bda59be272dc0efef6239f704228eb
Alena1234512 <109169107+Alena1234512@users.noreply.github.com>
2022-07-18 21:48:06 +0300

    Update DZ4.sql

--- DZ4.sql
+++ DZ4.sql
@@ -1,4 +1,4 @@
-create database DZ4_1;
+create database DZ4_2;
 create sequence provider_id;
 create table provider
 (
@@ -10,11 +10,8 @@ provider_address varchar
 insert into provider (provider, provider_address)
 values
 ('ООО Ромашка', 'г. Буржуев, ул. Пушкина д.8'),
-('ООО Ромашка', 'г. Буржуев, ул. Пушкина д.8'),
-('ООО Ромашка', 'г. Буржуев, ул. Пушкина д.8'),
 ('ООО Березка', 'г. Грехов, ул. Ломоносова д.190'),
 ('ООО Березка', 'г. Павлодар, ул. Ленина д.1');
-
 
 create sequence type_payment_id;
 
@@ -22,7 +19,6 @@ create table type_payment
 (
 id int not null default nextval('type_payment_id') primary key,
 type_playm varchar
-
 );
 
 
@@ -30,9 +26,8 @@ insert into type_payment (type_playm)
 values
 ('Наложенный платеж'),
 ('Безналичный платеж'),
-('Наличный платеж'),
-('Наличный платеж'),
-('Безналичный платеж');
+('Наличный платеж');
+
 
 create sequence customer_id;
 
@@ -40,15 +35,10 @@ create table customer (
 id int not null default nextval('customer_id') primary key,
 customer varchar,
 customer_address varchar
-
 );
 
 insert into customer (customer, customer_address)
 values
-('ООО Рога и Копыта', 'г. Деревянный, ул. Смирнова, д.4'),
-('ООО Рога и Копыта', 'г. Деревянный, ул. Смирнова, д.4'),
-('ООО Рога и Копыта', 'г. Деревянный, ул. Смирнова, д.4'),
-('ООО Рога и Копыта', 'г. Деревянный, ул. Смирнова, д.4'),
 ('ООО Рога и Копыта', 'г. Деревянный, ул. Смирнова, д.4');
 
 create sequence product_id;
@@ -65,10 +55,11 @@ customer_id int references customer(id),
 type_payment_id int references type_payment(id)
 );
 
-insert into product (name, quantity_provider, price, quantity_customer)
+insert into product (name, quantity_provider, price, quantity_customer, 
+provider_id, customer_id, type_payment_id)
 values
-('Зубная паста', 100, 25, 100),
-('Зубная нить', 34, 300, 34),
-('Ручки шариковые', 55, 12, 55),
-('Вода минеральная', 2, 150, 2),
-('Вода минеральная', 350, 500, 300);
+('Зубная паста', 100, 25, 100, 1, 1, 1),
+('Зубная нить', 34, 300, 34, 1, 1, 2),
+('Ручки шариковые', 55, 12, 55, 1, 1, 3),
+('Вода минеральная', 2, 150, 2, 2, 1, 3),
+('Вода минеральная', 350, 500, 300, 3, 1, 2);
